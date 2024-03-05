7696b379d49a35709e9ca1e5331eea01f502f656
Shalini <159447708+shalini-9262@users.noreply.github.com>
2024-03-05 13:30:06 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = "shalini"
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*"33" => 33
+"33abc" => NaN
+true => 1
+false => 0*/
+
+let isLoggedIn = "shalini"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*  1 => true;  0 => false
+"" => false
+"shalini" => true     */
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
