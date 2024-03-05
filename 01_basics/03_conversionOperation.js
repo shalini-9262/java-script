39b44da9b69cf7da9e6d4fae1907eefc84f73477
Shalini <159447708+shalini-9262@users.noreply.github.com>
2024-03-05 14:15:12 +0000

    Conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,7 @@ false => 0*/
 let isLoggedIn = "shalini"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /*  1 => true;  0 => false
 "" => false
@@ -27,5 +27,34 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+/*******************  Operations  *******************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(4+4);
+// console.log(4-4);
+// console.log(4*4);
+// console.log(4**4);
+// console.log(4/4);
+// console.log(4%9);
+
+let str1 = "Hello"
+let str2 = " Shalini"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+console.log(+true);
+console.log(true);
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
