a1923c718af5d05f2971531823755ea9abe0a666
Shalini <159447708+shalini-9262@users.noreply.github.com>
2024-03-24 15:29:10 +0000

    02_Variables

---
+++ 02_Varibles/script.js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the javascript")
+var a = 67 // a contains 67
+console.log(a)
+a = "Shalini"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7
