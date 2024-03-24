5853d2793ad1aad4e8cecc3cf20217740edf187d
Shalini <159447708+shalini-9262@users.noreply.github.com>
2024-03-24 17:05:48 +0000

    const, let and var in js

---
+++ 03_var_let_const/script.js
@@ -0,0 +1,17 @@
+console.log("Javascript : var, let, const");
+// var a = 45;
+// var a = "q";
+let b = "Shalini";
+ b =4;
+let c = null;
+let d = undefined;
+const author = "Shalini";
+//author = 5 // throws an erroe because constant cannot be changed.
+const Shalini = 0;
+{
+    let b = "this";
+    console.log(b);
+}
+console.log(b);
+// console.log(typeof c);
+console.log(Shalini);
